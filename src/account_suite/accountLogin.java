e1c7022d1d8de8e9e7661f0cd57b4c601abcbd61
Brett Lilley <brettlilley.j@gmail.com>
2014-12-01 16:33:23 -0500

    added account test suite

---
+++ src/account_suite/accountLogin.java
@@ -0,0 +1,105 @@
+package account_suite;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class accountLogin {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.thinkgeek.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAccountLogin() throws Exception {
+    driver.get(baseUrl + "/");
+    try {
+      assertEquals("ThinkGeek | Join In. Geek Out.", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.linkText("LOG IN")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | olapicFrame | ]]
+    //driver.findElement(By.id("olapicSubmit")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+    try {
+      assertEquals("Account Login | ThinkGeek", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.cssSelector("div.ninecol.lastcol > input[name=\"un\"]")).clear();
+    driver.findElement(By.cssSelector("div.ninecol.lastcol > input[name=\"un\"]")).sendKeys("brettlilley@comcast.net");
+    driver.findElement(By.cssSelector("div.ninecol.lastcol > input[name=\"pass\"]")).clear();
+    driver.findElement(By.cssSelector("div.ninecol.lastcol > input[name=\"pass\"]")).sendKeys("password1699");
+    driver.findElement(By.xpath("//input[@value='Log In']")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | olapicFrame | ]]
+    //driver.findElement(By.id("olapicSubmit")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+    try {
+      assertEquals("Your Account | ThinkGeek", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    // Warning: assertTextPresent may require manual changes
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*$"));
+    driver.findElement(By.linkText("LOG OUT")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | olapicFrame | ]]
+    //driver.findElement(By.id("olapicSubmit")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+    assertEquals("Log into your account", driver.findElement(By.cssSelector("h1.title.title-page")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
